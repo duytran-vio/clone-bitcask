5e022fcc91ff1517dcb8a6e6109ba9c11d4304f7
Duy Tran Duc <ducduyvio@gmail.com>
2025-12-03 08:49:35 +0700

    deserialize bitcask entry

--- bitcaskDB/entry.go
+++ bitcaskDB/entry.go
@@ -1,23 +1,61 @@
 package bitcaskdb
 
+import (
+	"bytes"
+	"encoding/binary"
+	"hash/crc32"
+	"time"
+)
+
+const (
+	CRC32_SIZE      = 4
+	TIMESTAMP_SIZE  = 8
+	KEY_SIZE        = 4
+	VALUE_SIZE      = 4
+	KEY_MAX_CHARS   = 2147483647
+	VALUE_MAX_CHARS = 2147483647
+)
+
 type Entry struct {
 	crc32     uint32
+	timestamp int64
 	keySize   int
 	valueSize int
 	key       string
 	value     string
+	data      []byte
 }
 
 func NewEntry(key, value string) *Entry {
+	timestamp := time.Now().Unix()
+	keySize := len(key)
+	valueSize := len(value)
+
+	data := toBytes(timestamp, keySize, valueSize, key, value)
+	crc32Value := crc32.ChecksumIEEE(data)
 	return &Entry{
-		crc32:     0, // Placeholder for CRC32 calculation
+		crc32:     crc32Value,
+		timestamp: timestamp,
 		keySize:   len(key),
 		valueSize: len(value),
 		key:       key,
 		value:     value,
+		data:      data,
 	}
 }
 
+func toBytes(timestamp int64, keySize, valueSize int, key, value string) []byte {
+	buf := new(bytes.Buffer)
+
+	binary.Write(buf, binary.LittleEndian, timestamp)
+	binary.Write(buf, binary.LittleEndian, int32(keySize))
+	binary.Write(buf, binary.LittleEndian, int32(valueSize))
+	buf.Write([]byte(key))
+	buf.Write([]byte(value))
+
+	return buf.Bytes()
+}
+
 func (e *Entry) Size() int {
-	return 4 + 4 + 4 + e.keySize + e.valueSize // crc32 + keySize + valueSize + key + value
+	return CRC32_SIZE + TIMESTAMP_SIZE + KEY_SIZE + VALUE_SIZE + e.keySize + e.valueSize
 }
